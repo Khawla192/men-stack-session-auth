b2d37014090517face73622b0d908f32ae1ba9d3
Khawla192 <202005171@stu.uob.edu.bh>
2024-12-29 21:06:07 +0300

    finish the sign-in

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 require('./config/database')
 const express = require("express")
+const session = require('express-session')
 
 const app = express()
 const methodOverride = require("method-override")
@@ -9,26 +10,32 @@ const morgan = require("morgan")
 // CONTROLLER
 const authCtr= require("./controllers/auth.js")
 
-
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000"
 
 // MIDDLEWARE
-
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }))
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"))
 // Morgan for logging HTTP requests
 app.use(morgan('dev'))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
 
 // PUBLIC ROUTES
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", {
+        user: req.session.user,
+    })
 })
 
 app.use("/auth", authCtr)
-
 
 // PROTECTED ROUTES
 
